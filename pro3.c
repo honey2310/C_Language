4d6f4f4de9a1ee9d0d807e79445d0e61d6320854
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2025-12-29 21:17:33 +0530

    Add programs for area and perimeter calculations
    Added multiple programs to calculate areas and simple interest.

---
+++ pro3.c
@@ -0,0 +1,75 @@
+//1. WAP to Find area of circle.
+// #include<stdio.h>
+
+// void main(){
+//     int r=5;
+//     float pie=3.14;
+    
+//     float result = pie*r*r;
+    
+//     printf("Area of circle is %.2f",result);
+// }
+
+
+//2. WAP to Find area of sqare.
+// #include<stdio.h>
+
+// void main(){
+//     int a=23;
+//     int result=a*a;
+    
+//     printf("Area of square is %d",result);
+// }
+
+//3. WAP to Find area of rectangle.
+// #include<stdio.h>
+
+// void main(){
+//     float width=12.3;
+//     float length=23.4;
+    
+//     float result=width*length;
+    
+//     printf("Area of rectangle is %.2f",result);
+// }
+
+//4. WAP to Find area of triangle.
+// #include <stdio.h>
+
+// void main() {
+//     float base, height, area;
+
+//     base = 10;
+//     height = 5;
+
+//     area = (base * height) / 2;
+
+//     printf("Area of triangle is %.2f", area);
+// }
+
+//5. WAP to Simple interest.
+// #include <stdio.h>
+
+// void main() {
+//     float p = 1000;
+//     float r = 5;
+//     float t = 2;
+//     float si;
+
+//     si = (p * r * t) / 100;
+
+//     printf("Simple Interest is %.2f", si);
+// }
+
+//6. WAP to Perimeter of circle.
+// #include <stdio.h>
+
+// void main() {
+//     int r = 5;
+//     float pi = 3.14;
+//     float perimeter;
+
+//     perimeter = 2 * pi * r;
+
+//     printf("Perimeter of circle is %.2f", perimeter);
+// }
