ea72539dc2dd82bc95e203f0148ac49c5afb266a
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2025-12-29 20:36:31 +0530

    Implement basic arithmetic operations in pro1.c

---
+++ pro1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+    int a=2;
+    int b=4;
+    int add=a+b;
+    int sub=b-a;
+    int multipy=a*b;
+    int division=b/a;
+    
+    printf("Addition: %d\n",add);
+    printf("Subtraction: %d\n",sub);
+    printf("Multiplcation: %d\n",multipy);
+    printf("Division: %d",division);
+}
