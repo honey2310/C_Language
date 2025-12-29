b64a7c951c3f03efcd6d7e833f93ecaa944c96dd
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2025-12-29 20:56:39 +0530

    Add programs for mathematical formula calculations
    This file contains multiple programs that calculate the results of various mathematical formulas involving addition and subtraction, including (x+y)^2, (x-y)^2, (x+y)^3, (x-y)^3, (x+y+z)^3, (x-y-z)^3, and (x+y+z)^2.

---
+++ pro2.c
@@ -0,0 +1,124 @@
+
+// 1.WAP to find a the answer given formula (x+y)2.
+// #include <stdio.h>
+
+// void main(){
+//     int a,b;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     int result=(a+b)*(a+b);
+//     printf("Result of (a+b)^2 is %d", result);
+// }
+
+
+
+// 2.WAP to find a the answer given formula (x-y)2.
+// #include <stdio.h>
+
+// void main(){
+//     int a,b;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     int result=(a-b)*(a-b);
+//     printf("Result of (a-b)^2 is %d", result);
+// }
+
+
+
+// 3.WAP to find a the answer given formula (x+y)3.
+// #include <stdio.h>
+
+// void main(){
+//     int a,b;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     int result=(a+b)*(a+b)*(a+b);
+//     printf("Result of (a+b)^3 is %d", result);
+// }
+
+
+
+// 4.WAP to find a the answer given formula (x-y)3.
+// #include <stdio.h>
+
+// void main(){
+//     int a,b;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     int result=(a-b)*(a-b)*(a-b);
+//     printf("Result of (a-b)^3 is %d", result);
+// }
+
+
+// 5.WAP to find a the answer given formula (x+y+z)3.
+// #include <stdio.h>
+
+// void main(){
+//     int a,b,c;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     printf("enter c:");
+//     scanf("%d",&c);
+    
+//     int result=(a+b+c)*(a+b+c)*(a+b+c);
+//     printf("Result of (a+b+c)^3 is %d", result);
+// }
+
+
+// 6.WAP to find a the answer given formula (x-y-z)3.
+// #include <stdio.h>
+
+// void main(){
+//     int a,b,c;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     printf("enter c:");
+//     scanf("%d",&c);
+    
+//     int result=(a-b-c)*(a-b-c)*(a-b-c);
+//     printf("Result of (a-b-c)^3 is %d", result);
+// }
+
+
+// 7.WAP to find a the answer given formula (x+y+z)2.
+// #include <stdio.h>
+
+// void main(){
+//     int a,b,c;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     printf("enter c:");
+//     scanf("%d",&c);
+    
+//     int result=(a+b+c)*(a+b+c);
+//     printf("Result of (a+b+c)^2 is %d", result);
+// }
+
