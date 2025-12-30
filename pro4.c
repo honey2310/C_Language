72840e187b0f770f99e0a05e63401fb076cb2b34
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2025-12-30 18:36:55 +0530

    Add commented code for minimum and number sign checks

---
+++ pro4.c
@@ -0,0 +1,34 @@
+// #include <stdio.h>
+
+// void main(){
+//     int a,b;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     printf("enter b:");
+//     scanf("%d",&b);
+    
+//     if(a<b){
+//         printf("a is minimum");
+//     }else{
+//          printf("b is minimum");
+//     }
+// }
+
+
+
+// #include<stdio.h>
+
+// void main(){
+//     int a;
+//     printf("enter a:");
+//     scanf("%d",&a);
+    
+//     if(a==0){
+//         printf("a is neutral");
+//     }else if(a<0){
+//         printf("a is negative");
+//     }else{
+//         printf("c is postive");
+//     }
+// }
