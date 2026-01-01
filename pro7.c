30d3cda4e60d5de7d39b659567b4218c2dfc9289
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-01 10:03:17 +0530

    Add multiple C program examples as comments
    This file contains multiple commented-out C programs demonstrating various functionalities such as printing numbers, checking for leap years, calculating sums, and finding factorials.

---
+++ pro7.c
@@ -0,0 +1,160 @@
+// 1 to 10
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+    
+//     while(i<=10){
+//         printf("%d ",i);
+//         i++;
+//     }
+// }
+
+
+//  10 to 1
+// #include<stdio.h>
+
+// void main(){
+//     int i=10;
+    
+//     while(i>=1){
+//         printf("%d ",i);
+//         i--;
+//     }
+// }
+
+
+// // 1 to n
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     while(i<=n){
+//         printf("%d ",i);
+//         i++;
+//     }
+// }
+
+
+// // n to 1
+// #include<stdio.h>
+
+// void main(){
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     while(n>=1){
+//         printf("%d ",n);
+//         n--;
+//     }
+// }
+
+
+// // odd number between 1 to n
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     while(i<=n){
+//         if(i%2==1){
+//           printf("%d ",i);
+//         }
+//         i++;
+//     }
+// }
+
+
+// even number between n to 1
+// #include<stdio.h>
+
+// void main(){
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     while(n>=1){
+//         if(n%2==0){
+//           printf("%d ",n);
+//         }
+//         n--;
+//     }
+// }
+
+// leap year 2000 to 3000
+// #include <stdio.h>
+
+// int main() {
+//     int year = 2000;
+
+//     while (year <= 3000) {
+//         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+//             printf("%d\n", year);
+//         }
+//         year++;
+//     }
+
+//     return 0;
+// }
+
+
+// sum of 1 to n
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int sum = 0;
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     while(i<=n){
+//         sum+=i;
+//         i++;
+//     }
+//     printf("Sum: %d",sum);
+// }
+
+
+
+// factorial of 1 to n
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n;
+//     int factorial=1;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     while(i<=n){ // 1<=5
+//         factorial*=i; // 
+//         i++;
+//     }
+//     printf("%d",factorial);
+// }
+
+
+// 5*1=5
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n,multiplcation;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     while(i<=10){
+//         multiplcation=n*i;
+//         printf("%d * %d = %d\n" , n,i,multiplcation);
+//         i++;
+// }
+// }
