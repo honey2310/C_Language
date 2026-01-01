4b12641533a3963952971038755aa03f550b6151
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-01 10:26:14 +0530

    Create pro8.c

---
+++ pro8.c
@@ -0,0 +1,146 @@
+
+// 1 to 10
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+    
+//     for(i;i<=10;i++){
+//         printf("%d ",i);
+//     }
+// }
+
+
+// 10 to 1
+// #include<stdio.h>
+
+// void main(){
+//     int i=10;
+    
+//     for(i;i>=1;i--){
+//         printf("%d ",i);
+//     }
+// }
+
+// 1to n
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     for(i;i<=n;i++){
+//         printf("%d ",i);
+//     }
+// }
+
+
+// n to 1
+
+// #include<stdio.h>
+
+// void main(){
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     for(n;n>=1;n--){
+//         printf("%d ",n);
+//     }
+// }
+
+
+// odd number from 1 to n
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     for(i;i<=n;i++){
+//       if(i%2==1){
+//         printf("%d ",i);
+//       }
+// }
+// }
+
+
+// n to 1 even number
+// #include<stdio.h>
+
+// void main(){
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     for(n;n>=1;n--){
+//       if(n%2==0){
+//         printf("%d ",n);
+//       }
+//     }
+// }
+
+
+// 2000 to 3000 leap year
+// #include<stdio.h>
+
+// void main(){
+//     int i;
+//     for(i=2000; i<=3000; i++){
+//         if((i%4==0 && i%100!=0) || i%400==0)
+//         printf("%d\n",i);
+//     }
+// }
+
+// 1 to n sum
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n;
+//     int sum =0;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     for(i;i<=n;i++){
+//         sum+=i;
+//     }
+//      printf("%d ",sum);
+// }
+
+// factorial
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int factorial=1;
+//     int n;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     for(i; i<=n; i++){
+//         factorial*=i;
+//     }
+//      printf("%d",factorial);
+// }
+
+
+// // 5*1=5
+// #include<stdio.h>
+
+// void main(){
+//     int i=1;
+//     int n,multiplcation;
+//     printf("enter n:");
+//     scanf("%d",&n);
+    
+//     for(i;i<=10;i++){
+//         multiplcation=n*i;
+//         printf("%d * %d = %d\n" , n,i,multiplcation);
+// }
+// }
+
