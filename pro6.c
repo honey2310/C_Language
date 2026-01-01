518f3a517680c9b41a5c797926b5e5824bbed1cf
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-01 09:30:24 +0530

    Add commented out telecom service and month switch cases

---
+++ pro6.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+// void main()
+// {
+//     int lang, service;
+
+//     printf("Welcome to Telecom Service\n");
+//     printf("Press 1 for English\n");
+//     printf("Press 2 for Hindi\n");
+//     printf("Enter your choice: ");
+//     scanf("%d", &lang);
+
+//     switch(lang)
+//     {
+//         case 1:
+//             printf("\nEnglish Selected\n");
+//             printf("Press 1 for Balance Enquiry\n");
+//             printf("Press 2 for Data Plans\n");
+//             scanf("%d", &service);
+
+//             switch(service)
+//             {
+//                 case 1:
+//                     printf("Your balance is Rs. 120\n");
+//                     break;
+
+//                 case 2:
+//                     printf("You have 2GB data remaining\n");
+//                     break;
+
+//                 default:
+//                     printf("Invalid Service Choice\n");
+//             }
+//             break;
+
+//         case 2:
+//             printf("\nHindi Selected\n");
+//             printf("Balance ke liye 1 dabaye\n");
+//             printf("Data Plans ke liye 2 dabaye\n");
+//             scanf("%d", &service);
+
+//             switch(service)
+//             {
+//                 case 1:
+//                     printf("Aapka balance Rs. 120 hai\n");
+//                     break;
+
+//                 case 2:
+//                     printf("Aapke paas 2GB data baki hai\n");
+//                     break;
+
+//                 default:
+//                     printf("Galat Choice\n");
+//             }
+//             break;
+
+//         default:
+//             printf("Invalid Language Choice\n");
+//     }
+// }
+
+
+
+
+
+#include <stdio.h>
+
+// void main()
+// {
+//     int month;
+
+//     printf("Enter month number (1-12): ");
+//     scanf("%d", &month);
+
+//     switch(month)
+//     {
+//         case 1:  printf("January"); break;
+//         case 2:  printf("February"); break;
+//         case 3:  printf("March"); break;
+//         case 4:  printf("April"); break;
+//         case 5:  printf("May"); break;
+//         case 6:  printf("June"); break;
+//         case 7:  printf("July"); break;
+//         case 8:  printf("August"); break;
+//         case 9:  printf("September"); break;
+//         case 10: printf("October"); break;
+//         case 11: printf("November"); break;
+//         case 12: printf("December"); break;
+
+//         default:
+//             printf("Invalid Month Number");
+//     }
+// }
+
