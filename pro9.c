275b80b0e23d6bc6b304a6e10990b794c3b30e74
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-04 11:37:44 +0530

    Update pro9.c

--- pro9.c
+++ pro9.c
@@ -138,3 +138,157 @@
 //     return 0;
 // }
 
+// Online C compiler to run C program online
+// 1.5, 2.3, 3.1, 3.9,
+// #include <stdio.h>
+
+// int main() {
+//     float a = 1.5;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 6; i++) {
+//         printf("%.1f ", a);
+//         a = a + 0.8;
+//     }
+
+//     return 0;
+// }
+
+
+// 14, 28, 20, 40, 32, 64,
+// #include <stdio.h>
+
+// int main() {
+//     int a = 14;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 8; i++) {
+//         printf("%d ", a);
+
+//         if(i % 2 != 0)
+//             a = a * 2;
+//         else
+//             a = a - 8;
+//     }
+
+//     return 0;
+// }
+
+
+//  2, 4, 6, 8, 10, 
+// #include <stdio.h>
+
+// int main() {
+//     int a = 2;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 7; i++) {
+//         printf("%d ", a);
+//         a = a + 2;
+//     }
+
+//     return 0;
+// }
+
+
+// 201, 202, 204, 207, 
+// #include <stdio.h>
+
+// int main() {
+//     int a = 201;
+//     int diff = 1;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 6; i++) {
+//         printf("%d ", a);
+//         a = a + diff;
+//         diff++;
+//     }
+
+//     return 0;
+// }
+
+
+// 544, 509, 474, 439, 
+// #include <stdio.h>
+
+// int main() {
+//     int a = 544;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 6; i++) {
+//         printf("%d ", a);
+//         a = a - 35;
+//     }
+
+//     return 0;
+// }
+
+
+//  80, 10, 70, 15, 60, 
+// #include <stdio.h>
+
+// int main() {
+//     int odd = 80;
+//     int even = 10;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 4; i++) {
+//         printf("%d ", odd);
+//         printf("%d ", even);
+
+//         odd = odd - 10;
+//         even = even + 5;
+//     }
+
+//     return 0;
+// }
+
+
+// 2, 6, 18, 54, 
+// #include <stdio.h>
+
+// int main() {
+//     int a = 2;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 6; i++) {
+//         printf("%d ", a);
+//         a = a * 3;
+//     }
+
+//     return 0;
+// }
+
+
+// 5.2, 4.8, 4.4, 4, 
+// #include <stdio.h>
+
+// int main() {
+//     float a = 5.2;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 6; i++) {
+//         printf("%.1f ", a);
+//         a = a - 0.4;
+//     }
+
+//     return 0;
+// }
+
+
+// 8, 6, 9, 23, 87 ,
+// #include <stdio.h>
+
+// int main() {
+//     int a = 8;
+
+//     printf("Series: ");
+//     for(int i = 1; i <= 6; i++) {
+//         printf("%d ", a);
+//         a = (a * i) - (i + 1);
+//     }
+
+//     return 0;
+// }
+
