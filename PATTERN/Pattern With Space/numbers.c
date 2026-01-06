d1774f35b74a4b5ac64df45f06c926756d638e55
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-06 22:49:47 +0530

    Add pattern printing C programs in numbers.c
    Added multiple C programs to print patterns using asterisks.

---
+++ PATTERN/Pattern With Space/numbers.c
@@ -0,0 +1,191 @@
+// Online C compiler to run C program online
+// 0
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if((i==1 && j==2)|| (i==1 && j==3)||(i==7 && j==2)|| (i==7 && j==3)|| (i==2 && j==1) || (i==2 && j==5)|| (i==3 && j==1) || (i==3 && j==5)|| (i==4 && j==1) || (i==4 && j==5)|| (i==5 && j==1) || (i==5 && j==5)|| (i==6 && j==1) || (i==6 && j==5)){
+//             printf("* ");
+//       }else{
+//           printf(" ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// 1
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if((i==2&&j==1)|| j==4 ||i==7){
+//             printf("* ");
+//       }else{
+//           printf(" ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// // 2
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if(i==1 || i==4 || i==7 || (i==2 && j==5)|| (i==3 && j==5)|| (i==4 && j==5)|| (i==6 && j==1)|| (i==5 && j==1)){
+//             printf("* ");
+//       }else{
+//           printf("  ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+// 3
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if(i==1 || i==4 || i==7 || (i==2 && j==5)|| (i==3 && j==5)|| (i==4 && j==5)|| (i==6 && j==5)|| (i==5 && j==5)){
+//             printf("* ");
+//       }else{
+//           printf("  ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// 4
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if(i==4 || (i==1&&j==1)|| (i==2&&j==1)|| (i==3&&j==1)|| (i==2&&j==5)|| (i==3&&j==5)|| (i==5&&j==5)||(i==6&&j==5)|| (i==7&&j==5)){
+//             printf("* ");
+//       }else{
+//           printf("  ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// 5
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if(i==1 || i==4 || i==7 || (i==2 && j==1)|| (i==3 && j==1)|| (i==4 && j==1)|| (i==6 && j==5)|| (i==5 && j==5)){
+//             printf("* ");
+//       }else{
+//           printf("  ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// 6
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if(j==1 || i==4 ||(i==4 && j==1)|| (i==6 && j==5)|| (i==5 && j==5)||(i==6 && j==5)|| i==7){
+//             printf("* ");
+//       }else{
+//           printf("  ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// 7
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if(i==1 || j==5){
+//             printf("* ");
+//       }else{
+//           printf("  ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// 8
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if((i==1 && j==2)|| (i==1 && j==3)||(i==4 && j==2)||(i==7 && j==2)|| (i==7 && j==3)|| (i==2 && j==1) || (i==2 && j==5)|| (i==3 && j==1) || (i==3 && j==5)|| (i==4 && j==1) || (i==4 && j==5)|| (i==5 && j==1) || (i==5 && j==5)|| (i==6 && j==1) || (i==6 && j==5)){
+//             printf("* ");
+//       }else{
+//           printf(" ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+// 9
+// #include <stdio.h>
+
+// int main(){
+//     int i,j;
+//     for(i=1;i<=7;i++){
+//     for(j=1;j<=5;j++){
+//       if(j==5 || i==4 ||(i==2 && j==1)|| (i==3 && j==1)|| (i==5 && j==5)||(i==6 && j==5)|| i==1){
+//             printf("* ");
+//       }else{
+//           printf("  ");
+//       }
+//     }
+//     printf("\n");
+// }
+//     return 0;
+// }
+
+
+
