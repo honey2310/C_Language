917fa8fbb7fd07ec8f9d608c346747018948e24d
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-07 12:37:36 +0530

    Create countDigit.c

---
+++ PR-TASK/countDigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Special case for 0
+    if (num == 0) {
+        count = 1;
+    } else {
+        while (num != 0) {
+            num = num / 10;
+            count++;
+        }
+    }
