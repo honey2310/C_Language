0024b4f547f10c245c21c05d04dbb9c3d70de2e3
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-08 17:56:10 +0530

    Add program to identify and print negative numbers
    This program takes an integer input for the size of an array, populates it with user input, and prints both the entire array and the negative numbers within it.

---
+++ ARRAY/negativeNo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n,i,j;
+    printf("enter n:");
+    scanf("%d",&n);
+    int a[n];
+    
+    for(i=0;i<n;i++){
+        printf("enter a[%d]: ",i);
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0; i<n; i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    for(i=0; i<n; i++){
+       if(a[i]<0){
+            printf(" %d ",a[i]);
+       }
+    }
+    
+    
+    return 0;
+}
