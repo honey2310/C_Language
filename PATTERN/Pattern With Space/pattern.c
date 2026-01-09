fc9fb410087e7b6f07a7ab70b630a4e7b6154a52
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-09 16:06:18 +0530

    Add pattern.c with various number patterns

---
+++ PATTERN/Pattern With Space/pattern.c
@@ -0,0 +1,215 @@
+//         1 
+//       1 2 
+//     1 2 3 
+//   1 2 3 4 
+// 1 2 3 4 5 
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+        
+//         for(k=i;k<5;k++){
+//             printf("  ");
+//         }
+//         for(j=1;j<=i;j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+//         1 
+//       2 1 
+//     3 2 1 
+//   4 3 2 1 
+// 5 4 3 2 1 
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+        
+//         for(k=i;k<5;k++){
+//             printf("  ");
+//         }
+//         for(j=i;j>=1;j--){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// 5 4 3 2 1 
+//   4 3 2 1 
+//     3 2 1 
+//       2 1 
+//         1
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=5;i>=1;i--){
+        
+//         for(k=i;k<5;k++){
+//             printf("  ");
+//         }
+//         for(j=i;j>=1;j--){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+//         5 
+//       4 5 
+//     3 4 5 
+//   2 3 4 5 
+// 1 2 3 4 5
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=5;i>=1;i--){
+        
+//         for(k=1;k<i;k++){
+//             printf("  ");
+//         }
+//         for(j=i;j<=5;j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+//         5 
+//       5 4 
+//     5 4 3 
+//   5 4 3 2 
+// 5 4 3 2 1 
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=5;i>=1;i--){
+        
+//         for(k=1;k<i;k++){
+//             printf("  ");
+//         }
+//         for(j=5;j>=i;j--){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+//         1 
+//       2 2 
+//     3 3 3 
+//   4 4 4 4 
+// 5 5 5 5 5 
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+        
+//         for(k=i;k<5;k++){
+//             printf("  ");
+//         }
+//         for(j=1;j<=i;j++){
+//             printf("%d ",i);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+//         5 
+//       4 4 
+//     3 3 3 
+//   2 2 2 2 
+// 1 1 1 1 1 
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=5;i>=1;i--){
+        
+//         for(k=1;k<i;k++){
+//             printf("  ");
+//         }
+//         for(j=i;j<=5;j++){
+//             printf("%d ",i);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// 1 2 3 4 5 
+//   2 3 4 5 
+//     3 4 5 
+//       4 5 
+//         5 
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+        
+//         for(k=1;k<i;k++){
+//             printf("  ");
+//         }
+//         for(j=i;j<=5;j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// 1 2 3 4 5 
+//   1 2 3 4 
+//     1 2 3 
+//       1 2 
+//         1
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=5;i>=1;i--){
+        
+//         for(k=5;k>i;k--){
+//             printf("  ");
+//         }
+//         for(j=1;j<=i;j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// 5 4 3 2 1 
+//   5 4 3 2 
+//     5 4 3 
+//       5 4 
+//         5 
+// #include<stdio.h>
+
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+        
+//         for(k=i;k>1;k--){
+//             printf("  ");
+//         }
+//         for(j=5;j>=i;j--){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
