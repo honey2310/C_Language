6fbb1827b72fd390c1540acce2e07d6a2eb70d3e
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-09 15:31:07 +0530

    Add pattern printing program in pattern5.c
    This program prints a pattern of numbers in a specific format.

---
+++ PATTERN/Custom/pattern5.c
@@ -0,0 +1,42 @@
+// 1 2 3 4 5 5 4 3 2 1 
+// 1 2 3 4     4 3 2 1 
+// 1 2 3         3 2 1 
+// 1 2             2 1 
+// 1                 1 
+// 1 2             2 1 
+// 1 2 3         3 2 1 
+// 1 2 3 4     4 3 2 1 
+// 1 2 3 4 5 5 4 3 2 1 
+
+#include<stdio.h>
+
+int main(){
+    int i,j,k,l;
+    
+    for(i=5;i>=1;i--){
+    for(j=1;j<=i;j++){
+        printf("%d ",j);
+    }
+    for(k=i;k<5;k++){
+        printf("    ");
+    }
+    for(l=i;l>=1;l--){
+        printf("%d ",l);
+    }
+    printf("\n");
+    }
+    for(i=2;i<=5;i++){
+    for(j=1;j<=i;j++){
+        printf("%d ",j);
+    }
+    for(k=i;k<5;k++){
+        printf("    ");
+    }
+    for(l=i;l>=1;l--){
+        printf("%d ",l);
+    }
+    printf("\n");
+    }
+    
+    return 0;
+}
