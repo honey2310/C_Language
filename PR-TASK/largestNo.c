deb7a78e48592397050d46a5518ebc58a4a8fb0b
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-01-19 14:29:36 +0530

    Create largestNo.c

---
+++ PR-TASK/largestNo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+
+    printf("Enter elements of the matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int max = arr[0][0];
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            if(arr[i][j] > max) {
+                max = arr[i][j];
+            }
+        }
+    }
+
+    printf("Largest element = %d\n", max);
+
+    return 0;
+}
+
